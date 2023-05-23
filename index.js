27d154fef997a19fd7fbd9ecef5639df7854660e
ajaythopate <ajaythopate999@gmail.com>
2023-05-23 09:29:53 +0530

    add new blog functionality

--- index.js
+++ index.js
@@ -30,3 +30,57 @@ function fetchBlogs() {
         });
 }
 fetchBlogs();
+
+
+// Add a new blog
+function addBlog() {
+    const blogTitleInput = document.getElementById('blogTitleInput');
+    const blogContentInput = document.getElementById('blogContentInput');
+
+    const blogTitle = blogTitleInput.value;
+    const blogContent = blogContentInput.value;
+
+    if (blogTitle.trim() === '' || blogContent.trim() === '') {
+        alert('Please enter a title and content for the blog.');
+        return;
+    }
+
+    const newBlog = {
+        title: blogTitle,
+        body: blogContent,
+    };
+
+    // Send the new blog to the server (not implemented in this example)
+    console.log('New blog:', newBlog);
+
+    const blogsContainer = document.getElementById('blogsContainer');
+
+    // Create the new blog card
+    const blogCard = document.createElement('div');
+    blogCard.classList.add('blogCard');
+
+    const blogTitleElement = document.createElement('h2');
+    blogTitleElement.textContent = blogTitle;
+
+    const blogContentElement = document.createElement('p');
+    blogContentElement.textContent = blogContent;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('deleteButton');
+
+    blogCard.appendChild(blogTitleElement);
+    blogCard.appendChild(blogContentElement);
+    blogCard.appendChild(deleteButton);
+
+    // Insert the new blog card at the beginning of the container
+    blogsContainer.insertBefore(blogCard, blogsContainer.firstChild);
+
+    // Clear the input fields
+    blogTitleInput.value = '';
+    blogContentInput.value = '';
+}
+
+
+// Event listeners
+document.getElementById('addBlogButton').addEventListener('click', addBlog);
